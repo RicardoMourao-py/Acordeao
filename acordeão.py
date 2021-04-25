cd9a0a9be749446552d542afdb4f72167f880900
Ricardo Mourão-py <ricardomrf@al.insper.edu.br>
2021-04-25 07:10:43 -0300

    Explicação do jogo

--- acordeão.py
+++ acordeão.py
@@ -1,3 +1,16 @@
 # Começando o exercício progama
 print('Design de Software')
 print('Ricardo Mourão Rodrigues Filhos')
+
+# Explicação do Acordeão
+print('Paciência Acordeão')
+print('=================== \n')
+print('Seja bem-vindo(a) ao jogo de Paciência Acordeão! O objetivo deste jogo é colocar todas as cartas em uma mesma pilha. \n')
+print('Existem apenas dois movimentos possíveis: \n')
+print('1. Empilhar uma carta sobre a carta imediatamente anterior; ')
+print('2. Empilhar uma carta sobre a terceira carta anterior. \n')
+print('Para que um movimento possa ser realizado basta que uma das duas condições abaixo seja atendida: \n')
+print('1. As duas cartas possuem o mesmo valor ou ')
+print('2. As duas cartas possuem o mesmo naipe. \n')
+print('Desde que alguma das condições acima seja satisfeita, qualquer carta pode ser movimentada. \n')
+input('Aperte [Enter] para iniciar o jogo... ')
